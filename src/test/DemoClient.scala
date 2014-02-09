7a179e3ae520ad16b1368508dac8bc76c9f5c5e6
Dan Fox <iamdanfox@gmail.com>
2014-02-09 22:41:19 +0000

    Proof of concept working in eclipse.
    Debugging two simultaneously seems to work.

---
+++ src/test/DemoClient.scala
@@ -0,0 +1,11 @@
+package test
+
+object DemoClient {
+
+  def main(args: Array[String]): Unit = {
+    println("client started")
+    Thread.sleep(5000);
+    println("client stopped")
+  }
+
+}
---
+++ src/test/DemoServer.scala
@@ -0,0 +1,11 @@
+package test
+
+object DemoServer {
+
+  def main(args: Array[String]): Unit = {
+    println("Server started")
+    Thread.sleep(10000)
+    println("Server stopped")
+  }
+
+}
