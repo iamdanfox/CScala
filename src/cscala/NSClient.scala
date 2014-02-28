ddff2888cdf4e752dae217a5adeddc9fe2d5d9c8
Dan Fox <iamdanfox@gmail.com>
2014-02-28 15:42:02 +0000

    Error when the same service is registered twice on two different local JVMs

--- src/cscala/NSClient.scala
+++ src/cscala/NSClient.scala
@@ -2,16 +2,24 @@ package cscala
 
 import java.net.InetAddress
 
-
 object NSClient {
 
   // This client will attempt to look up the address of the 'DummyEntry' service  
   def main(args: Array[String]): Unit = {
     println("Client started")
-    
+
     NS().register("DummyService", InetAddress.getByName("localhost"), 3301)
-    
+
     println(NS().lookup("DummyService"))
+/*
+ * Exception in thread "main" ox.cso.Closed: OneOne-1
+at ox.cso.SyncChan.$bang(SyncChan.scala:98)
+at ox.cso.OutPort$Proxy$class.$bang(OutPort.scala:116)
+at ox.cso.Connection$ProxyServer.$bang(Connection.scala:101)
+at cscala.ForeignNSWrapper.lookup(ForeignNSWrapper.scala:22)
+at cscala.NSClient$.main(NSClient.scala:14)
+at cscala.NSClient.main(NSClient.scala)
+ */
   }
 
 }
