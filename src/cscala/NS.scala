68c830984aad979b8e4e087240d7d5503cf3657c
Dan Fox <iamdanfox@gmail.com>
2014-02-28 14:46:04 +0000

    Refactored NS

--- src/cscala/NS.scala
+++ src/cscala/NS.scala
@@ -9,53 +9,43 @@ import java.net._
 import java.net.ConnectException
 
 /**
- * Listen
- * Allow services to register (either by listening over a particular port,
- * or through internal JVM communication).
- * Allow processes to lookup services.
+ * Returns a nameserver.
+ *
+ * Returns the locally running one if possible, otherwise, will find one running on this JVM.
+ *
+ * Starts a new local NameServer if no other options.
  */
 object NS {
 
   private var impl: NameServer = null
 
-  def isRunning(): Boolean = this.impl!=null
-  
-  /**
-   * Returns a NameServer.  Finds one on a local JVM, finds one on this JVM or starts a new one.
-   */
-  def apply(): NameServer = {
-    if (!isRunning()) {
-      try {
-          System.out.println("NS() trying to connect to local JVM")
-          val nameServer = NetIO.clientConnection[Msg, Msg]("localhost", 7700, false)
-          // wrap the foreign JVM
-          impl = new ForeignNSWrapper(nameServer)
-          return impl
-      } catch {
-        // no nameserver running on a local JVM
-        case ce : java.net.ConnectException => {
-          println("NS() starting a new local NameServer")
-          // start a new one!
-          impl = new LocalNS()
-          return impl
-        }
-      } 
-    } else {
+  def isRunning(): Boolean = this.impl != null
+
+  def apply(): NameServer = { // TODO is this threadsafe?
+    if (isRunning()) {
       // local nameserver is already running
       println("NS() Already running locally")
-      impl
+      return impl
+    } else findForeignNS() match {
+      case Some(foreignNS) =>
+        System.out.println("NS() trying to connect to local JVM")
+        // wrap the foreign server object with `register`, `lookup` methods
+        impl = new ForeignNSWrapper(foreignNS)
+        return impl
+      case None =>
+        println("NS() starting a new local NameServer")
+        // start a new one!
+        impl = new LocalNS()
+        return impl
     }
   }
 
-  //  def main(args: Array[String]) = {
-  //    val ns = NS();
-  //  }
-
-  //    // dummy insertion
-  //    (proc {
-  //      val respCh = OneOne[Boolean];
-  //      // TODO: PORT!
-  //      putCh ! (("DummyService", InetAddress.getByName("localhost"), respCh))
-  //      println("Entered DummyService: " + (respCh?))
-  //    })();
+  private def findForeignNS(): Option[ox.cso.NetIO.Server[Msg, Msg]] = {
+    try {
+      return Some(NetIO.clientConnection[Msg, Msg]("localhost", 7700, false))
+    } catch {
+      // couldn't connect to localhost:7700, 
+      case ce: java.net.ConnectException => return None
+    }
+  }
 }
