2055075411d3d18353760f905c671d1d5cfafb86
Dan Fox <iamdanfox@gmail.com>
2014-04-15 15:07:16 +0100

    EchoService seems to work

--- src/demo/EchoService.scala
+++ src/demo/EchoService.scala
@@ -11,37 +11,39 @@ import java.net.InetAddress
  * A trivial service that simply echoes back the message it receives then terminates the client.
  */
 object EchoService {
-  
+
   val port = 3302
-  protected var thread = null.asInstanceOf[ox.cso.ThreadHandle]
-  
-  private def startEchoService(){
-       this.thread = NetIO.serverPort(port, 0, false, handleClient).fork
+
+  private def startEchoService() {
+    NetIO.serverPort(port, 0, false, handleClient).fork
   }
-  
-  private def stopEchoService(){
-    this.thread.interrupt // this is messy
-  }
-  
+
   private def handleClient(client: NetIO.Client[String, String]) = {
-      proc("Handling EchoService request"){
-          val incoming = (client?)
-          client!("Echo: "+incoming)
-      }.fork
+    proc("Handling EchoService request") {
+      val incoming = (client?)
+      client !(incoming)
+    }.fork
   }
-  
+
   def main(args: Array[String]): Unit = {
     // start listening over the internet
     startEchoService()
-    
+
     // register with nameserver
     NS().register("EchoService", InetAddress.getByName("localhost"), this.port)
-    
+
+    // pretend to be a client.
     NS().lookup("EchoService") match {
-      case Some((addr,port)) => {
-        // TODO: send something, print out the response
+      case Some((addr, p)) => {
+        // foreign server
+        val server = NetIO.clientConnection[String, String](addr, p, false)
+        println("sending   'Hello'")
+        server ! "Hello"
+        println("receiving '"+(server?)+"'")
       }
       case None => println("This shouldn't have happened.")
     }
+
+    // must send Ctrl-C interrupt to stop this.
   }
 }
