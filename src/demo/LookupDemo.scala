7977c5328139e4680a00182c4cb29be047dd87ff
Dan Fox <iamdanfox@gmail.com>
2014-04-15 14:08:11 +0100

    Consolidating demos

---
+++ src/demo/LookupDemo.scala
@@ -0,0 +1,26 @@
+package demo
+
+import cscala.NS
+import java.net.InetAddress
+
+/**
+ * Registers an address with the nameserver.  Performs unsuccessful and successful lookups.
+ */
+object LookupDemo {
+
+  val myport = 7701;
+
+  /*
+   * Run this twice.  First instance will start a NameServer.  Second running instance will connect to the first one's nameserver.
+   */
+  def main(args: Array[String]): Unit = {
+
+    val ns = NS()
+
+    println(ns.lookup("Demo")) // will print "None" on the first attempt
+    ns.register("Demo", InetAddress.getByName("localhost"), 3301)
+    println(ns.lookup("Demo")) // will print "Some((localhost/127.0.0.1,3301))" on the first attempt
+
+  }
+
+}
