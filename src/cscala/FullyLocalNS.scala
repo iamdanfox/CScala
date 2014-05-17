e1492c3222cbdee61be01d7f96170b87bc14ebdf
Dan Fox <iamdanfox@gmail.com>
2014-05-17 20:40:01 +0100

    Half finished FullyLocalNS
    Planning on refactoring Registry class

--- src/cscala/FullyLocalNS.scala
+++ src/cscala/FullyLocalNS.scala
@@ -5,15 +5,43 @@ import ox.cso.Connection._
 
 class FullyLocalNS {
   
+  private val registry = new collection.mutable.HashMap[String,(Client[Any,Any] => Unit, Registry.Timestamp, NameServer.TTL)]()
 
-  def register[A, B](name: String, ttl: NameServer.TTL, handleClient: (Client[A, B]) => Unit): Boolean = {
-    return false;
-  }
-    
+  def register[Req, Resp](name: String, ttl: NameServer.TTL, handleClient: (Client[Req, Resp]) => Unit): Boolean = {
+    registry.get(name) match {
+      case Some((handlefn, timestamp, ttl)) if timestamp+ttl > System.currentTimeMillis() => {
+        
+      }
+      case None => {
+         return false
+      }
+    }
+  }  
+  
   /**
-   * Lookup a name in the nameserver, throw an exception otherwise.
+   * Lookup a name in the nameserver and 'connect' to the service. Throw an exception otherwise.
    */
-  def lookup[A,B](name: String) : Server[A,B] = {
-    
+  def lookup[Req,Resp](name: String) : Server[Req,Resp] = synchronized {
+    lookup2[Req,Resp](name) match {
+      case Some(server) => return server
+      case None => throw new NameServer.NameNotFoundException(name)
+    }
   }
+  
+  /**
+   * Tentative lookup and connect
+   */
+  def lookup2[Req,Resp](name: String) : Option[Server[Req,Resp]] = synchronized {
+    registry.get(name) match {
+      case Some(handlefn) => {
+        val handleFunction = handlefn.asInstanceOf[Client[Req,Resp] => Unit]
+        // 'connect' to the server
+        val conn = ox.cso.Connection.OneOne[Req,Resp]
+        proc { handleFunction(conn.client) }.fork 
+        return Some(conn.server);
+      }
+      case None => return None
+    }
+  }
+  
 }
