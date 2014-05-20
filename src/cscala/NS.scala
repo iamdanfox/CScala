30ec14386ba05dae114aa12cdd5871a505a2e99f
Dan Fox <iamdanfox@gmail.com>
2014-05-20 21:59:26 +0100

    Formatting

--- src/cscala/NS.scala
+++ src/cscala/NS.scala
@@ -8,16 +8,17 @@ import ox.cso.Connection._
 object NS extends Connectable {
 
   private val ns = new UDPDistributedNS()
-  
+
   val nameServerAddress = InetAddress.getLocalHost()
 
-  def lookup(name:String):Option[(InetAddress, Int)] = ns.lookupAddr(name)
-  
-  def connect[Req, Resp](name: String): Option[Server[Req, Resp]] = ns.lookup[Req, Resp](name)
+  def lookup(name: String): Option[(InetAddress, Int)] =
+    ns.lookupAddr(name)
 
-  def register(name: String, v: (InetAddress, Int), ttl: Long): Boolean = {
+  def connect[Req, Resp](name: String): Option[Server[Req, Resp]] =
+    ns.lookup[Req, Resp](name)
+
+  def register(name: String, v: (InetAddress, Int), ttl: Long): Boolean =
     ns.registerAddr(name, v._1, v._2, ttl)
-  }
 }
 
 /**
@@ -27,11 +28,11 @@ object LocalNS extends Connectable {
 
   private val ns = new FullyLocalNS()
 
-  def connect[Req, Resp](name: String): Option[Server[Req, Resp]] = ns.lookup2[Req, Resp](name)
+  def connect[Req, Resp](name: String): Option[Server[Req, Resp]] =
+    ns.lookup2[Req, Resp](name)
 
-  def register[Req, Resp](name: String, handleClient: Client[Req, Resp] => Unit, ttl: Long): Boolean = {
+  def register[Req, Resp](name: String, handleClient: Client[Req, Resp] => Unit, ttl: Long): Boolean =
     ns.register[Req, Resp](name, ttl, handleClient)
-  }
 }
 
 object DualNS extends Connectable {
@@ -39,17 +40,15 @@ object DualNS extends Connectable {
   /**
    * Does local lookup first, then attempts a foreign one.
    */
-  def connect[Req, Resp](name: String): Option[Server[Req, Resp]] = LocalNS.connect[Req, Resp](name) match {
-    case Some(s) => Some(s)
-    case None => NS.connect[Req, Resp](name)
-  }
+  def connect[Req, Resp](name: String): Option[Server[Req, Resp]] =
+    LocalNS.connect[Req, Resp](name) match {
+      case Some(s) => Some(s)
+      case None => NS.connect[Req, Resp](name)
+    }
 
-  def register[Req, Resp](name: String, handleClient: Client[Req, Resp] => Unit, ttl: Long): Boolean = {
-    LocalNS.register[Req,Resp](name, handleClient, ttl)
-  }
-  
-  def register(name:String, v: (InetAddress, Int), ttl: Long) : Boolean = {
+  def register[Req, Resp](name: String, handleClient: Client[Req, Resp] => Unit, ttl: Long): Boolean =
+    LocalNS.register[Req, Resp](name, handleClient, ttl)
+
+  def register(name: String, v: (InetAddress, Int), ttl: Long): Boolean =
     NS.register(name, v, ttl)
-  }
-
 }
